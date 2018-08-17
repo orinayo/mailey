1618e8850146375f158a92da21ab0cdeaf011c1b
orinayo <orinayooyelade@gmail.com>
2018-08-17 02:04:08 +0100

    added billing and client side

--- client/src/components/LandingPage/LandingPage.test.js
+++
@@ -1,7 +0,0 @@
-import { shallow } from 'enzyme'
-import React from 'react'
-import LandingPage from './LandingPage'
-
-it('expect to render LandingPage component', () => {
-  shallow(<LandingPage />).html()
-})
